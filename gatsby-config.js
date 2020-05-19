ed5944c9790d3dbf1470b4e6625f522ad6b65375
tibtof <tibtof@gmail.com>
2020-05-19 18:15:07 +0300

    smaller map

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,49 @@
-const config = ({ basePath = `/`, mdx = true }) => ({
-  siteMetadata: {
-    siteTitle: `rogojeni`,
-    siteTitleAlt: `rogojeni`,
-    siteHeadline: `Alianta Rogojeni`,
-    siteUrl: `https://rogojeni.ro`,
-    siteDescription: `Home Page for Rogojeni Alliance`,
-    siteLanguage: `ro`,
-    siteImage: `/banner.jpg`,
-    author: `Tiberiu Tofan`,
-    basePath,
-  },
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `sections`,
-        path: `${__dirname}/src/sections`,
-      },
+const config = ({basePath = `/`, mdx = true}) => ({
+    siteMetadata: {
+        siteTitle: `rogojeni`,
+        siteTitleAlt: `rogojeni`,
+        siteHeadline: `Alianta Rogojeni`,
+        siteUrl: `https://rogojeni.ro`,
+        siteDescription: `Home Page for Rogojeni Alliance`,
+        siteLanguage: `ro`,
+        siteImage: `/banner.jpg`,
+        author: `Tiberiu Tofan`,
+        basePath,
     },
-    mdx && {
-      resolve: `gatsby-plugin-mdx`,
-      options: {},
-    },
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-typescript`,
-    `gatsby-plugin-emotion`,
-    `gatsby-plugin-theme-ui`,
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [`gatsby-remark-responsive-iframe`],
-      },
-    },
-  ],
+    plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `sections`,
+                path: `${__dirname}/src/sections`,
+            },
+        },
+        mdx && {
+            resolve: `gatsby-plugin-mdx`,
+            options: {
+                remarkPlugins: [{
+                    resolve: `gatsby-remark-responsive-iframe`,
+                    options: {}
+                }]
+            },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [{
+                    resolve: `gatsby-remark-responsive-iframe`,
+                    options: {
+                        wrapperStyle: `margin-bottom: 1.0725rem`
+                    }
+                }
+                ],
+            },
+        },
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-typescript`,
+        `gatsby-plugin-emotion`,
+        `gatsby-plugin-theme-ui`,
+    ],
 });
 
-module.exports = config({ basePath: `/`, mdx: true });
+module.exports = config({basePath: `/`, mdx: true});
